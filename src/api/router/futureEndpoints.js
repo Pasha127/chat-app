1766e9227f79e59a050a3e59d8fe342e08dbe400
Pasha127 <pasha.account@gmail.com>
2022-11-24 15:29:58 +0100

    update

--- src/api/router/futureEndpoints.js
+++ src/api/router/futureEndpoints.js
@@ -2,8 +2,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary"; */
 
-/* import passport from "passport"; */
-
 /* const cloudinaryUploader = multer({
   storage: new CloudinaryStorage({
     cloudinary, 
@@ -18,20 +16,6 @@ import { CloudinaryStorage } from "multer-storage-cloudinary"; */
 
 
 
-/* router.get("/googleLogin", passport.authenticate("google",{scope:["email","profile"]})) */
-
-/* router.get("/googleRedirect", passport.authenticate("google",{session: false}), async (req, res, next) => {
-
-  try {
-    const {accessToken,refreshToken} = req.user
-    res.cookie("accessToken",accessToken,{"httpOnly":true});
-    res.cookie("refreshToken",refreshToken,{"httpOnly":true});
-    res.redirect(`${process.env.FE_DEV_URL}/`/* ?loginSuccessful=true *//* );
-  }catch(error){
-    console.log(error)
-      next(error);
-  }   
-}) */
 
 /* router.get("/facebookLogin", passport.authenticate('facebook')) */
 
--- src/socket/index.js
+++ src/socket/index.js
@@ -17,8 +17,8 @@ export const newConnectionHandler = (newClient) => {
   });
   newClient.on("setUsername", (payload) => {
     onlineUsers.push({ username: payload.username, socketId: newClient.id });
-    newClient.emit("loggedIn", onlineUsers);
-    newClient.broadcast.emit("listUpdate", onlineUsers);
+    /* newClient.broadcast.emit("loggedIn", onlineUsers); */
+   io.emit("listUpdate", onlineUsers);
     console.log(onlineUsers);
   });
 
@@ -27,7 +27,7 @@ export const newConnectionHandler = (newClient) => {
 
     console.log("this is incoming message", socket.content);
     const msg = new MessageModel(socket.message);
-    console.log("this is saved message", msg);
+    //console.log("this is saved message", msg);
     const newMsg = await msg.save();
 
     const commonChat = await chatModel.find({
@@ -37,13 +37,13 @@ export const newConnectionHandler = (newClient) => {
       commonChat[0].messages.push(newMsg._id);
 
       await commonChat[0].save();
-      console.log(commonChat);
-      console.log("has chat");
+      //console.log(commonChat);
+      //console.log("has chat");
       const chatId = commonChat._id;
       newClient.join(chatId);
       io.to(chatId).emit("testmessage", socket.message.content);
     } else {
-      console.log("no chat");
+      //console.log("no chat");
       const newChat = new chatModel({
         members: socket.members,
         messages: [newMsg._id],
@@ -53,39 +53,7 @@ export const newConnectionHandler = (newClient) => {
       io.to(_id).emit("testmessage", socket.message.content);
     }
   });
-
-  /* io.on("startChat", (socket) => {
-    console.log("this is chat content: ", chat);
-    /*  const id = await chatModel.find({ members: [...chat.members] });
-    socket.join(`chatroom`);
-    newClient.emit("receive", { message: "this is content message" });
-    io.to("chatroom").emit("hi");
-  }); */
-
-  /* newClient.on("sendMessage", async (chat) => {
-   
-
-    try {
-        const commonChat = await chatModel.find({ members: chat.members });
-      //check if the send and receiver have a common chat
-      if (commonChat) {
-        //update the already existing chat with the new message
-        console.log("the members already exist");
-      } else {
-        //create a new chat for the 2 users and update it with the message
-        const newChat = new chatModel({
-          members: chat.members,
-          messages: chat.messages,
-        });
-        newChat.save();
-      } 
-
-      newClient.emit("newMessage", chat.message);
-      newClient.broadcast.emit("newMessage", chat.message);
-    } catch (error) {
-      console.log(error);
-    }
-  }); */
+ 
   newClient.on("disconnect", () => {
     onlineUsers = onlineUsers.filter((user) => user.socketId !== newClient.id);
     newClient.broadcast.emit("listUpdate", onlineUsers);
