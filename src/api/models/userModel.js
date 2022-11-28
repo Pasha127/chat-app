d6f2fb0cbba35d3b16bfba816645e076d20eb173
Pasha127 <pasha.account@gmail.com>
2022-11-28 21:55:10 +0100

    no unique users

--- src/api/models/userModel.js
+++ src/api/models/userModel.js
@@ -4,8 +4,8 @@ import bcrypt from "bcrypt";
 const userDBSchema = new Schema(
   {
     password: { type: String },
-    email: { type: String, required: true },
-    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     avatar: { type: String, default: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" },
     refreshToken: { type: String },
   },
